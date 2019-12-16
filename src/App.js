4dbf4b6f43dda2e4a7ecdacd03358309e248b533
Vishwas <vishwasblaze@gmail.com>
2019-12-16 16:27:16 +0530

    Added render and Hardcoded Lists

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="checkbox"/>
+      <p>List 1</p>
+      <input type="checkbox"/>
+      <p>List 2</p>
+      <input type="checkbox"/>
+      <p>List 3</p>
     </div>
   );
 }
