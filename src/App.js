e059445041fcce423a0795aa5a18580201c3d362
Vishwas <vishwasblaze@gmail.com>
2019-12-20 22:57:18 +0530

    Changed to Class based Component , State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoItem from './TodoItem'
 import './todoItem.css'
 import todosData from './todosData'
 
-const App = () => {
-  console.log(todosData)
-  const todoItems = todosData
-  .map(
-    item => <TodoItem key={item.id} item={item}/> 
-  )
-  return (
-    <div className="todolist">  
-      {todoItems}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+  render() {
+    const todoItems = this.state.todos
+    .map(
+      item => <TodoItem key={item.id} item={item}/> 
+    )
+    return (
+      <div className="todolist">  
+        {todoItems}
+      </div>
+    );
+        
+  }
 }
 
 export default App;
