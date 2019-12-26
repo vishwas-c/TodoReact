5f20695b75f10ebde00522665940b3224ddd5b80
Vishwas <vishwasblaze@gmail.com>
2019-12-26 09:57:26 +0530

    Added style for ToDo final Version

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 function TodoItem (props) {
+  const completedStyle = {
+    fontStyle : "Italic",
+    color: "gray",
+    textDecoration: "line-through"
+  }
   console.log(props)
   return (
     <div className="todoitem">
       <input type="checkbox" 
       checked={props.item.completed} 
       onChange={() => props.clickHandler(props.item.id)} />
-      <p>{props.item.text}</p>
+      <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
     </div>
   )
 }
